5e4c4e9f87ec807e04a970b91859d085a5919b27
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2020-10-21 11:51:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var logo = document.querySelector('nav .logo');
 var menu = document.querySelector('.menu');
-var menu_links = document.querySelector('.menu ul');
+var menu_links = document.querySelector('.one');
 
 
 logo.addEventListener('click' , function(){
     
-  if (menu.style.height === "40%") {
+  if (menu.style.height == "25%") {
     menu.style.height = '0%';
     menu_links.style.display = 'none';
   } else {
-    menu.style.height = "40%";
-    menu_links.style.display = 'initial';
+    menu.style.height = "25%";
+    menu_links.style.display = 'block';
   }
 });
